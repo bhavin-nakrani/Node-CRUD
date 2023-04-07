521c0a064ba5cbd12110123cbba2e3c0c64d06c4
Bhavin Nakrani <bhavin.it8488@gmail.com>
2023-04-07 13:00:24 +0530

    Add container name

--- userdirectory/database.js
+++ userdirectory/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || '',
+    process.env.DB_USER || 'root',
+    process.env.DB_PASSWORD || 'admin@123',
     {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 5432,
